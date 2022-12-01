dbfaf24e35674fe3375f95cba14d04bec3d92cdb
witek <witus2706@gmail.com>
2022-12-01 19:29:55 +0100

    cleanup + add further steps description

--- src/main/scala/wsoczek/project/streams/Config.scala
+++ src/main/scala/wsoczek/project/streams/Config.scala
@@ -25,6 +25,4 @@ object Config {
         )
     )
   }
-
 }
-
--- src/test/scala/SubStreamProcessorSpec.scala
+++ src/test/scala/SubStreamProcessorSpec.scala
@@ -4,19 +4,22 @@ import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.should.Matchers
 import wsoczek.project.streams.SubStreamProcessor
 
-class SubStreamProcessorSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers {
+class SubStreamProcessorSpec
+    extends AsyncFreeSpec
+    with AsyncIOSpec
+    with Matchers {
 
   "Substream processor " - {
     "valid flow" in {
-      Stream(1,2,3,4)
-        .broadcastThrough(SubStreamProcessor(2).flow:_*)
+      Stream(1, 2, 3, 4)
+        .broadcastThrough(SubStreamProcessor(2).flow: _*)
         .compile
         .toList
-        .asserting(_ should contain theSameElementsAs List((0, 6), (1,4)))
+        .asserting(_ should contain theSameElementsAs List((0, 6), (1, 4)))
     }
     "empty stream" in {
       Stream()
-        .broadcastThrough(SubStreamProcessor(2).flow:_*)
+        .broadcastThrough(SubStreamProcessor(2).flow: _*)
         .compile
         .toList
         .asserting(_ should contain theSameElementsAs List())
